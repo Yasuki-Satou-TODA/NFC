f96be81f3f6833abaaa66c5d47715da9d4c22f4b
ikawashima-cq <i.kawashima@caraquri.com>
2021-05-23 09:40:08 +0900

    Initial Commit

---
+++ toda-nfc-reader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toda-nfc-reader
+//
+//  Created by Iichiro Kawashima on 2021/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
